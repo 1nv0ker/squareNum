51fc03fd0fc5ce6939a3a75d34803085a629c103
FTP <760604256@qq.com>
2022-03-11 16:02:18 +0800

    md 修改 Signed-off-by: FTP <760604256@qq.com>

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-import { getDatas} from './index.js'
+import { getDatas} from './db.js'
 getDatas().then(res => {
     console.log(Math.min(...res.map(item=>item.count)))//最小次数
     console.log(Math.max(...res.map(item=>item.count)))//最大次数
