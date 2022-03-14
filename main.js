9bd8686da3b46dc002ad2c009f317ca275664c62
FTP <760604256@qq.com>
2022-03-14 15:04:33 +0800

    添加了三个数平方和求值的功能 Signed-off-by: FTP <760604256@qq.com>

--- main.js
+++ main.js
@@ -1,34 +1,17 @@
 import { insertDatas } from './db.js'
 
-let total = 97//平方和
+let total = 94//平方和
 let sqrtNum = parseInt(Math.sqrt(total))+1//平方根
 let runCount = 0
-let runTime = 10000//总样本量
-async function countSquare() {
+let runTime = 10//总样本量
+async function countSquare() {//
     let x = getRndInteger(1, sqrtNum)
-    let y = getRndInteger(sqrtNum - x, sqrtNum)
+    let y = 1
     let count = 0
     while(!(((total%2) === (x+y)%2) && (x*x+y*y) === total)) {
         x = getRndInteger(1, sqrtNum)
-        let temp = getRndInteger(sqrtNum - x, sqrtNum)
-        if (total%2 === 0) {
-            if (x%2===0) {
-                y = temp%2===0?temp:(temp-1)
-            } else {
-                y = temp%2===0?temp-1:temp
-            }
-        } else {
-            if (x%2===0) {
-                y = temp%2===0?temp-1:temp
-            } else {
-                y = temp%2===0?temp:temp-1
-            }
-        }
+        y = parseInt(Math.sqrt(total-x*x))
         count++
-        // if (count>total) {
-        //     console.log('error', x, y, count)
-        //     return false
-        // }
     }
     let data = {
         x: x,
@@ -37,15 +20,37 @@ async function countSquare() {
         total: total
     }
     await insertDatas(data)
-    // console.log('success', x, y, count, (x*x+y*y))
+    console.log('success', x, y, count, (x*x+y*y))
 }
-let runInterval = setInterval(() => {
-    countSquare()
-    runCount++
-    if (runCount>runTime) {
-        clearInterval(runInterval)
+// countSquare()
+countSquaer_three();
+function countSquaer_three() {//三个数的平方和
+    let x = getRndInteger(1, sqrtNum)
+    let y = getRndInteger(parseInt(Math.sqrt(total-x*x)), sqrtNum)
+    let z = 0
+    let count = 0
+    while((x*x+y*y+z*z) !== total) {
+        x = getRndInteger(1, sqrtNum)
+        y = getRndInteger(parseInt(Math.sqrt(total-x*x)), sqrtNum)
+        z = parseInt(Math.sqrt(total-x*x-y*y))
+        count++
     }
-}, 100);
+    // let data = {
+    //     x: x,
+    //     y: y,
+    //     count: count,
+    //     total: total
+    // }
+    // await insertDatas(data)
+    console.log('success', x, y, count, (x*x+y*y+z*z))
+}
+// let runInterval = setInterval(() => {
+//     countSquare()
+//     runCount++
+//     if (runCount>runTime) {
+//         clearInterval(runInterval)
+//     }
+// }, 100);
 function getRndInteger(min, max) {//随机函数
     return Math.floor(Math.random() * (max - min) ) + min;
 }
